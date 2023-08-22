183c29691d744ae124e7a0619441480307ff6ee4
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2023-08-22 12:42:31 +0530

    OUTPUT

--- 11.MST greedy.c
+++ 11.MST greedy.c
@@ -1,61 +1,65 @@
-#include <iostream>    
-#include <algorithm>    
-using namespace std;    
-const int MAX = 1e4 + 5;    
-int id[MAX], nodes, edges;    
-pair <long long, pair<int, int> > p[MAX];    
-void init()    
-{    
-    for(int i = 0;i < MAX;++i)    
-        id[i] = i;    
-}      
-int root(int x)    
-{    
-    while(id[x] != x)    
-    {    
-        id[x] = id[id[x]];    
-        x = id[x];    
-    }    
-    return x;    
-}      
-void union1(int x, int y)    
-{    
-    int p = root(x);    
-    int q = root(y);    
-    id[p] = id[q];    
-}     
-long long kruskal(pair<long long, pair<int, int> > p[])    
-{    
-    int x, y;    
-    long long cost, minimumCost = 0;    
-    for(int i = 0;i < edges;++i)    
-    {    
-        x = p[i].second.first;    
-        y = p[i].second.second;    
-        cost = p[i].first;    
-        if(root(x) != root(y))    
-        {    
-            minimumCost += cost;    
-            union1(x, y);    
-        }        
-    }    
-    return minimumCost;    
-}     
-int main()    
-{    
-    int x, y;    
-    long long weight, cost, minimumCost;    
-    init();    
-    cout <<"Enter Nodes and edges";    
-    cin >> nodes >> edges;    
-    for(int i = 0;i < edges;++i)    
-    {    
-        cout<<"Enter the value of X, Y and edges";    
-    cin >> x >> y >> weight;    
-        p[i] = make_pair(weight, make_pair(x, y));    
-    }    
-    sort(p, p + edges);    
-    minimumCost = kruskal(p);    
-    cout <<"Minimum cost is "<< minimumCost << endl;    
-    return 0;    
+#include <stdio.h>
+#include <stdlib.h>
+struct Edge {
+    int src, dest, weight;
+};
+struct Subset {
+    int parent;
+    int rank;
+};
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i) {
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    }
+    return subsets[i].parent;
+}
+void unionSets(struct Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+    if (subsets[xroot].rank < subsets[yroot].rank) {
+        subsets[xroot].parent = yroot;
+    } else if (subsets[xroot].rank > subsets[yroot].rank) {
+        subsets[yroot].parent = xroot;
+    } else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+int compareEdges(const void *a, const void *b) {
+    return ((struct Edge *)a)->weight - ((struct Edge *)b)->weight;
+}
+void kruskalMST(int V, int E, struct Edge edges[]) {
+    struct Subset *subsets = (struct Subset *)malloc(V * sizeof(struct Subset));
+    for (int i = 0; i < V; i++) {
+        subsets[i].parent = i;
+        subsets[i].rank = 0;
+    }
+    qsort(edges, E, sizeof(struct Edge), compareEdges);
+    struct Edge result[V];
+    int resultIndex = 0;
+    for (int i = 0; i < E && resultIndex < V - 1; i++) {
+        int srcRoot = find(subsets, edges[i].src);
+        int destRoot = find(subsets, edges[i].dest);
+        if (srcRoot != destRoot) {
+            result[resultIndex++] = edges[i];
+            unionSets(subsets, srcRoot, destRoot);
+        }
+    }
+    printf("Minimum Spanning Tree:\n");
+    for (int i = 0; i < resultIndex; i++) {
+        printf("%d - %d: %d\n", result[i].src, result[i].dest, result[i].weight);
+    }
+    free(subsets);
+}
+int main() {
+    int V, E;
+    printf("Enter the number of vertices and edges: ");
+    scanf("%d %d", &V, &E);
+    struct Edge edges[E];
+    printf("Enter source, destination, and weight for each edge:\n");
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
+    }
+    kruskalMST(V, E, edges);
+    return 0;
 }
