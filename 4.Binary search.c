d25729a04d7b78fdd591f25868ff9bbed0545be2
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2023-08-23 12:42:46 +0530

    OUTPUT

--- 4.Binary search.c
+++ 4.Binary search.c
@@ -1,7 +1,8 @@
-#include <stdio.h>  
+#include <stdio.h>
 int binarySearch(int arr[], int low, int high, int target) {
     while (low <= high) {
         int mid = low + (high - low) / 2;
+
         if (arr[mid] == target)
             return mid;
         else if (arr[mid] < target)
@@ -9,20 +10,54 @@ int binarySearch(int arr[], int low, int high, int target) {
         else
             high = mid - 1;
     }
-    return -1;      
-}   
-int main() {  
-  int a[] = {11, 14, 25, 30, 40, 41, 52, 57, 70};   
-  int target = 40;  
-  int n = sizeof(a) / sizeof(a[0]); 
-  int res = binarySearch(a, 0, n-1, target);  
-  printf("The elements of the array are - ");  
-  for (int i = 0; i < n; i++)  
-  printf("%d ", a[i]);   
-  printf("\nElement to be searched is - %d", target);  
-  if (res == -1)  
-  printf("\nElement is not present in the array");  
-  else  
-  printf("\nElement is present at %d position of array", res);  
-  return 0;  
-} 
+    return -1; 
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    int target;
+    printf("Enter the element to search for: ");
+    scanf("%d", &target);
+    int index = binarySearch(arr, 0, n - 1, target);
+    if (index != -1) {
+        printf("Element found at index: %d\n", index);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+    return 0;
+}
